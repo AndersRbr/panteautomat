1381c979287c767af9932383ecf09aa4db92815c
Anders Brown Ranvik <>
2025-06-23 12:48:33 +0200

    create topbanner component

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/automat.css";
+
+const Banner: React.FC = () => {
+  return (
+    <div className="banner">
+      <h2>🎉 35 MILLIONER I POTTEN</h2>
+      <p>PANT OG VINN – støtt Røde Kors</p>
+    </div>
+  );
+};
+
+export default Banner;
