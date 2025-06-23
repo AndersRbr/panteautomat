0aac87a710eb531e3efc1354c9916243b753b714
Anders Brown Ranvik <>
2025-06-23 19:39:11 +0200

    fix reciept

--- src/components/Receipt/Receipt.tsx
+++ src/components/Receipt/Receipt.tsx
@@ -15,16 +15,23 @@ const Receipt: React.FC<ReceiptProps> = ({ lastPant, onBack }) => {
   return (
     <section className="receipt" aria-label="Kvittering for pant">
       <header>
-        <h2>🧾 Kvittering</h2>
+        <h2>Kvittering</h2>
       </header>
-      <p>{lastPant.bottles + lastPant.cans} enheter</p>
-      <p>Totalt: {lastPant.sum.toFixed(2)} kr</p>
       <p>
         Dato:{" "}
         {lastPant.timestamp
           ? new Date(lastPant.timestamp).toLocaleString("no-NO")
           : "Ingen dato tilgjengelig"}
       </p>
+      <hr />
+      <h3>Coop Mega</h3>
+      <p>Waldemar Thranes gate 72</p>
+      <hr />
+      <p>{lastPant.bottles + lastPant.cans} enheter</p>
+      <h4 aria-label={`Totalbeløp: ${lastPant.sum.toFixed(2)} kroner`}>
+        Total: {lastPant.sum.toFixed(2)} kr
+      </h4>
+
       <button className="btn btn-primary" onClick={onBack}>
         Tilbake til automat
       </button>
