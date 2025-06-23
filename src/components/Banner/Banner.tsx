5525a17f5233d5092ab72198c02a50f96ab9a0d2
Anders Brown Ranvik <>
2025-06-23 20:16:39 +0200

    added h1

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -4,7 +4,8 @@ import "../Banner/Banner.css";
 const Banner: React.FC = () => {
   return (
     <div className="banner">
-      <h2>🎉 35 MILLIONER I POTTEN</h2>
+      <h1>Panteautomat</h1>
+
       <p>PANT OG VINN – støtt Røde Kors</p>
     </div>
   );
